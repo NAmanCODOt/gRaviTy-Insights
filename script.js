dcbb3474fde73b1bc79a8bbdd88f209921a48942
NAmanCODOt <ayiraditya09@gmail.com>
2026-04-22 12:27:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const faders = document.querySelectorAll(".fade");
 
-const observer = new IntersectionObserver(entries => {
+const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
     }
   });
+}, {
+  threshold: 0.2
 });
 
 faders.forEach(el => observer.observe(el));
